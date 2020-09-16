7c5a67853ec163145d86861f921ea488ec3f3cc4
Adam Bilski <abilski2013@gmail.com>
2020-09-16 13:51:43 -0500

    update

--- index.js
+++ index.js
@@ -12,18 +12,23 @@ d3.json('https://api.eia.gov/series/?api_key=d4ac61ba04c07e0632b7f4800ae9a869&se
     var newObj = [];
 
     for (i = 0; i<newdata.length; i++) {
-        newObj.push({"date":newdata[i][0], "value":newdata[i][1]});
+        newObj.push({"description":description,"date":newdata[i][0], "value":newdata[i][1]});
     };
 
     var obj = {
-        "Description": description,
         "Data": newObj
     };
     
-    console.log(obj);
-    var str = JSON.stringify(obj);
+    var tabledata = obj.Data;
 
-    console.log(str);
-    d3.select('body').append('html').html(`<h1>` + str + `</h1>`);
-  
+    for (var i in tabledata) {
+        var row = `<tr>
+                        <td>${tabledata[i].description}</td>
+                        <td>${tabledata[i].date}</td>
+                        <td>${tabledata[i].value}</td>
+                   </tr>`;
+        var table = $('#table-body')
+        table.append(row)
+        
+    };
 });
